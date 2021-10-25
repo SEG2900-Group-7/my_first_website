3980638615077ca51b5d62adc16598c077ec6406
Jay Ghosh <jayghosh730@gmail.com>
2021-10-24 22:50:12 -0400

    js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-var a = 1;
+
+//This is the javascript pop up funciton. This will be referenced in all pages. 
+function popup() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+    }
+
+//The other place we used javascript is on the home page to implement smooth scroll. The JS there is inline. 
