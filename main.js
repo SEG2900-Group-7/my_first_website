d55bd8a3029814fddcc7f6a554fb750869866a2f
Jay Ghosh <jayghosh730@gmail.com>
2021-10-27 14:01:24 -0400

    navbar responsive design

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ function popup() {
     }
 
 //The other place we used javascript is on the home page to implement smooth scroll. The JS there is inline. 
+
+//responsive navbar hamburger menu. 
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+})
